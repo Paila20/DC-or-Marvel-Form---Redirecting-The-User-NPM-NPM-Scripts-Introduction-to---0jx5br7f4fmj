40462d494c4768c6a3e70f0ab856d2df0d22b26f
paila20 <rajarajeswar169@gmail.com>
2024-01-05 11:59:34 +0530

    commit

--- app/FormA.js
+++ app/FormA.js
@@ -1,31 +1,38 @@
-'use client'
+"use client";
 import { useState } from "react";
-
-function FormA({ onSubmit, age }) {
-    const handleSubmit = (event) => {
-    };
-    return (
-        <form id="dc" onSubmit={handleSubmit}>
-            <h2>Form A</h2>
-            <label>
-                Select DC Shows:
-                <select  >
-                    <option value="">--Select--</option>
-                    <option value="The Flash">The Flash</option>
-                    <option value="Arrow">Arrow</option>
-                    <option value="Supergirl">Supergirl</option>
-                    <option value="Legends of Tomorrow">Legends of Tomorrow</option>
-                </select>
-            </label>
-            <br />
-            <label>
-                Enter your age:
-                <input type="number" value={age} disabled />
-            </label>
-            <br />
-            <button id="submit-dc" type="submit">Submit</button>
-
-        </form>
-    );
+function FormA({ showSummary, age }) {
+	const [dcShow, setDCShow] = useState("");
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		if (dcShow === "") return;
+		const data = { age, show: dcShow, formType: "A" };
+		showSummary(data);
+	};
+	return (
+		<form id="dc" onSubmit={handleSubmit}>
+			<h2>Form A</h2>
+			<label>
+				Select DC Shows:
+				<select onChange={(e) => setDCShow(e.target.value)}>
+					<option value="">--Select--</option>
+					<option value="The Flash">The Flash</option>
+					<option value="Arrow">Arrow</option>
+					<option value="Supergirl">Supergirl</option>
+					<option value="Legends of Tomorrow">
+						Legends of Tomorrow
+					</option>
+				</select>
+			</label>
+			<br />
+			<label>
+				Enter your age:
+				<input type="number" value={age} disabled />
+			</label>
+			<br />
+			<button id="submit-dc" type="submit">
+				Submit
+			</button>
+		</form>
+	);
 }
 export default FormA;
--- app/Summary.js
+++ app/Summary.js
@@ -1,17 +1,14 @@
-'use client'
+"use client";
 import { useState } from "react";
 function Summary({ formData }) {
-    return (
-        <div id="summary">
-            <h2>Summary</h2>
-            <p>Form Type: </p>
-            <p>Age: </p>
-
-            <p>DC Shows: </p>
-
-            <p>Marvel Shows: </p>
-
-        </div>
-    );
+	return (
+		<div id="summary">
+			<h2>Summary</h2>
+			<p>Form Type: Form {formData.formType}</p>
+			<p>Age: {formData.age}</p>
+			{formData.formType == "A" && <p>DC Shows: {formData.show}</p>}
+			{formData.formType == "B" && <p>Marvel Shows: {formData.show}</p>}
+		</div>
+	);
 }
 export default Summary;
